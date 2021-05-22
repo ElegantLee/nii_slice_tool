6a520239006414fbaf1bf7aff9e447c1ed748f3b
ElegantLee <superlee.handso@gmail.com>
2021-05-22 16:41:49 +0800

    lee-1907 commit again

--- mainwindow.py
+++ mainwindow.py
@@ -16,6 +16,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.cwd = os.getcwd()
+        self.output_path = {'coronal': 'D:/', 'axial': 'D:/', 'sagittal': 'D:/'}       # 输出文件的路径列表
 
         """ 按钮对应的逻辑函数"""
         # 点击open
@@ -23,13 +24,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # 点击open folder
         self.action_open_Folder.triggered.connect(self.open_folder)
         # 点击coronal的输出文件夹
-        self.coronal_path_toolButton.clicked.connect(self.coronal_path_lineEdit.clear)
+        self.coronal_path_toolButton.clicked.connect(self.set_coronal_path)
         # 点击axial的输出文件夹
-        self.axial_path_toolButton.clicked['bool'].connect(self.axial_path_lineEdit.clear)
+        self.axial_path_toolButton.clicked['bool'].connect(self.set_axial_path)
         # 点击sagittal的输出文件夹
-        self.sagittal_path_toolButton.clicked.connect(self.sagittal_path_lineEdit.clear)
+        self.sagittal_path_toolButton.clicked.connect(self.set_sagittal_path)
         # 点击slice button后，更新进度条
-        self.slice_button.clicked.connect(self.process_progressBar.reset)
+        self.slice_button.clicked.connect(self.do_slice)
         # 点击slice button后，更新log
         # self.slice_button.clicked.connect(self.log_textEdit)
 
@@ -43,3 +44,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file_dir = QFileDialog.getExistingDirectory(self, caption='open a directory',
                                                     directory='D:/')
         self.folder_path_lineEdit.setText(file_dir)
+
+    def set_coronal_path(self):
+        dir = QFileDialog.getExistingDirectory(self, 'set coronal slice output path', directory='D:/')
+        self.output_path['coronal'] = dir
+        self.coronal_path_lineEdit.setText(dir)
+
+
+    def set_axial_path(self):
+        dir = QFileDialog.getExistingDirectory(self, 'set axial slice output path', directory='D:/')
+        self.output_path['axial'] = dir
+        self.axial_path_lineEdit.setText(dir)
+
+    def set_sagittal_path(self):
+        dir = QFileDialog.getExistingDirectory(self, 'set sagittal slice output path', directory='D:/')
+        self.output_path['sagittal'] = dir
+        self.sagittal_path_lineEdit.setText(dir)
+
+    def do_slice(self):
+        pass
